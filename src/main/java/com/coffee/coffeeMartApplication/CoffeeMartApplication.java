fb4b6e7765f2e0df836c14da6d9f35585558a618
Akshay <asr.asrcode@gmail.com>
2018-12-09 18:18:03 -0500

    intial commit

---
+++ src/main/java/com/coffee/coffeeMartApplication/CoffeeMartApplication.java
@@ -0,0 +1,12 @@
+package com.coffee.coffeeMartApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeMartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeMartApplication.class, args);
+	}
+}
